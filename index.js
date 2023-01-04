dbc5a805285f63934bc4f9fdd4a90c002162348b
HasieEst <ayrtonm0@gmail.com>
2023-01-04 12:20:31 +0200

    "Displayed all articles and all previous assignments."

---
+++ index.js
@@ -0,0 +1,55 @@
+// Application packages
+const express = require('express')
+const app = express()
+
+const path = require('path')
+// add template engine
+const hbs = require('express-handlebars')
+//setup template engine directory and files extensions
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts'
+}))
+
+//setup static public directory
+app.use(express.static(path.join(__dirname, '/public')))
+
+const mysql = require('mysql')
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
+// create database connection
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "qwerty",
+    database: "joga_mysql"
+})
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to joga_mysql db")
+})
+
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render('index', {
+            articles: articles
+        })
+    })
+})
+
+//app start point
+
+app.listen(3000, () => {
+    console.log("App is started at http://localhost:3000")
+})
