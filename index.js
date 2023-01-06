2cfc502d75014e27bbd6e4c251249c8856c2bb60
HasieEst <ayrtonm0@gmail.com>
2023-01-06 14:02:45 +0200

    “Created author controller and route, refactored application code”

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Application packages
 const express = require('express')
 const app = express()
-// import stringToUrl
-
-
 const path = require('path')
 // add template engine
 const hbs = require('express-handlebars')
@@ -19,79 +16,15 @@ app.engine('hbs', hbs.engine({
 //setup static public directory
 app.use(express.static(path.join(__dirname, '/public')))
 
-const mysql = require('mysql')
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 
-// create database connection
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "qwerty",
-    database: "joga_mysql"
-})
-
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected to joga_mysql db")
-})
-// show all articles - index page
-app.get('/', (req, res) => {
-    let query = "SELECT * FROM article";
-    let articles = []
-    con.query(query, (err, result) => {
-        if (err) throw err;
-        articles = result
-        res.render('index', {
-            articles: articles
-        })
-    })
-})
-// show article by this slug
-app.get('/article/:slug', (req, res) => {
-    let query = `select a.*,
-                        au.name as author,
-                        au.id   as author_id
-                 from article a,
-                      author au
-                 where slug = "${req.params.slug}"
-                   and a.author_id = au.id`
-    let article
-    con.query(query, (err, result) => {
-        if (err) throw err
-        article = result
-        res.render('article', {
-            article: article
-        })
-    })
-})
-// show articles grouped by author
-
-app.get('/author/:author_id', (req, res) => {
-    let query = `select *
-                 from article
-                 where author_id = "${req.params.author_id}"`
-    let articles
-    let author
-    con.query(query, (err, result) => {
-        if (err) throw err
-        articles = result
-        query = `Select *
-                 from author
-                 where id = "${req.params.author_id}"`
-        con.query(query, (err, result) => {
-            if (err) throw err
-            author = result
-            console.log(author)
-            res.render('author', {
-                author: author,
-                articles: articles
-            })
-        })
-    })
-})
+const articleRoutes = require('./routes/article')
+const authorRoutes = require('./routes/author')
+app.use('/', articleRoutes)
+app.use('/article', articleRoutes)
+app.use('/author', authorRoutes)
 //app start point
 
 app.listen(3000, () => {
