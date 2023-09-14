8b0200beba093501adad2507f5c341545e62fa51
Ayrton Mägipõld <ayrtonm0@gmail.com>
2023-09-14 13:42:11 +0300

    “Displayed an article by slug using the article model”

--- controllers/article.js
+++ controllers/article.js
@@ -4,35 +4,32 @@ const con = require('../utils/db')
 
 // show all articles - index page
 const getAllArticles = (req, res) => {
-    Article.getAll((err,data)=>{
-        if(err){
+    Article.getAll((err, data) => {
+        if (err) {
             res.status(500).send({
                 message: err.message || 'Some error ocurred retrieving articles data'
             })
         } else {
             console.log(data)
             res.render('index', {
-                article: data
+                articles: data
             })
         }
     })
 }
 // show article by this slug
 const getArticlesBySlug = (req, res) => {
-    let query = `select a.*,
-                        au.name as author,
-                        au.id   as author_id
-                 from article a,
-                      author au
-                 where slug = "${req.params.slug}"
-                   and a.author_id = au.id`
-    let article
-    con.query(query, (err, result) => {
-        if (err) throw err
-        article = result
-        res.render('article', {
-            article: article
-        })
+    Article.getBySlug(req.params.slug, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: err.message || 'Some error ocurred retrieving articles data'
+            })
+        } else {
+            console.log(data)
+            res.render('article', {
+                article: data
+            })
+        }
     })
 }
 
--- models/article.model.js
+++ models/article.model.js
@@ -26,6 +26,28 @@ Article.getAll = (result) => {
     })
 }
 
+Article.getBySlug = (slug, result) => {
+    let query = `select a.*,
+                        au.name as author,
+                        au.id   as author_id
+                 from article a,
+                      author au
+                 where slug = "${slug}"
+                   and a.author_id = au.id`
+    let article
+    con.query(query, (err, res) => {
+        if(err) {
+            console.log('error: ', err)
+            result(err, null)
+            return
+        }
+        if (res.length) {
+            console.log('found article: ', res[0]);
+            result(null, res[0])
+        }
+    })
+}
+
 
 
 
